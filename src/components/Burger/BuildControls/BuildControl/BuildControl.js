755c443b31a75e3d0e16821679c7f281cb84677f
Daniel Pataki <ptamasdaniel92@gmail.com>
2021-01-28 16:54:07 +0100

    add remove ingredients logic

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -4,8 +4,8 @@ import styles from "./BuildControl.module.css";
 const buildControl = (props) => (
     <div className={styles.BuildControl}>
         <div className={styles.Label}>{props.label}</div>
-        <button className={styles.Less}>Less</button>
-        <button onClick={props.added} className={styles.More}>More</button>
+        <button className={styles.Less} onClick={props.removed} disabled={props.disabled}>Less</button>
+        <button className={styles.More} onClick={props.added}>More</button>
     </div>
 );
 
--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -12,7 +12,13 @@ const controls = [
 
 const buildControls = (props) => (
     <div className={styles.BuildControls}>
-        {controls.map((ctrl, i) => <BuildControl key={ctrl.label} label={ctrl.label} added={() => props.ingredientAdded(ctrl.type)}/>)}
+        {controls.map((ctrl, i) => <BuildControl 
+        key={ctrl.label} 
+        label={ctrl.label} 
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabled[ctrl.type]}
+    />)}  
     </div>
 )
 
